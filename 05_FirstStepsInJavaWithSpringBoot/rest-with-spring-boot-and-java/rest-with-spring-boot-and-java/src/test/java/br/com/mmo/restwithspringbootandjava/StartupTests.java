27c2cba8c0626700ee5501d23f5a798c562ae31e
Murillosys <murillosys@gmail.com>
2023-09-11 16:52:34 -0300

    Primeiro commit; Primeira chamada REST;

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/test/java/br/com/mmo/restwithspringbootandjava/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.mmo.restwithspringbootandjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
